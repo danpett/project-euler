d42efe4f2bf878d6356fd9d8737d25bca29d59e2
Daniel Pettersson <daniel.pettersson@cryptify.se>
2017-05-13 14:41:46 +0200

    Solved problem 1.

---
+++ 0001/daniel/puzzle.py
@@ -0,0 +1,10 @@
+def solve():
+    sum = 0
+    for x in range(1, 1000):
+        if x % 3 == 0 or x % 5 == 0:
+            sum += x
+
+    return sum
+
+if __name__ == "__main__":
+    print solve()
